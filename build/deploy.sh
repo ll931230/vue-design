6a6a33a34f176b9a14ce244fd3a83bb834922285
liulei <lei.liul@ymm56.com>
2021-11-11 10:16:59 +0800

    submit

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-npm run build
+npm run docs:build
 
-cd /
+cd docs/.vuepress/dist
 
 git init
 git config user.name 'liulei'
@@ -12,6 +12,6 @@ git config user.email 'lei.liul@ymm56.com'
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:liulei9311230/vue_design.git master:main
+git push -f git@github.com:liulei9311230/vue-design.git master:gh-pages
 
 cd -
