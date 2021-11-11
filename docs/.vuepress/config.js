a64efa764497e03df3469783b61873b87d3195a4
liulei <lei.liul@ymm56.com>
2021-11-11 17:24:05 +0800

    去掉最后一个

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,28 +17,9 @@ module.exports = {
         ['/zh/renderer-diff', '渲染器的核心 Diff 算法'],
         ['/zh/renderer-advanced', '自定义渲染器'],
         ['/zh/stateful-component', '有状态组件的设计'],
-        ['/zh/observer', '基于 Proxy 的响应系统'],
+        // ['/zh/observer', '基于 Proxy 的响应系统'],
     ],
   },
-  // themeConfig: {
-  //   // displayAllHeaders: true,
-  //   sidebarDepth: 2,
-  //   locales: {
-  //   },
-  //   // sidebar: [
-  //   //   ['/zh/essence-of-comp', '组件的本质'],
-  //   //   ['/zh/vnode', '先设计 VNode 吧'],
-  //   //   ['/zh/h', '辅助创建 VNode 的 h 函数'],
-  //   //   ['/zh/renderer', '渲染器之挂载'],
-  //   //   ['/zh/renderer-patch', '渲染器之patch'],
-  //   //   ['/zh/renderer-diff', '渲染器的核心 Diff 算法'],
-  //   //   ['/zh/renderer-advanced', '自定义渲染器'],
-  //   //   ['/zh/stateful-component', '有状态组件的设计'],
-  //   //   // ['/zh/observer', '基于 Proxy 的响应系统'],
-  //   //   // ['/zh/component-expand', '组件的拓展']
-  //   // ],
-  //   docsDir: 'docs'
-  // },
   configureWebpack: {
     resolve: {
       alias: {
